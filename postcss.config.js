44ff5cd7cb72fcc2e13cf890ac5d58537f95b777
Kaili Kameoka <kaili.kameoka@gmail.com>
2023-10-25 14:37:04 -1000

    Added support for postcss variables and nesting.

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import postcssSimpleVars from 'postcss-simple-vars';
+import postcssNesting from 'postcss-nesting';
+
+export default {
+	plugins: [
+		postcssSimpleVars,
+		postcssNesting,
+	],
+};
