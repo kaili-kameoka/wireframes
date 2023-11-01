dd02cdfa0bd24eb0bf54f431e7f0b357c702dd68
Kaili Kameoka <kaili.kameoka@gmail.com>
2023-11-01 13:45:20 -1000

    Refactored mixins Added all other colors.

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,13 @@
 import postcssSimpleVars from 'postcss-simple-vars';
 import postcssNesting from 'postcss-nesting';
 import postcssComment from 'postcss-comment'
+import postcssMixins from 'postcss-mixins';
+
 export default {
 	plugins: [
 		postcssSimpleVars,
 		postcssNesting,
+			postcssMixins
 	],
 	parser: postcssComment
 };
