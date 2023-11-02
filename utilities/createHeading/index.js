7beb289ebc6bd3722fe6b38e6dd3bf6e769893ef
Kaili Kameoka <kaili.kameoka@gmail.com>
2023-11-02 13:44:19 -1000

    Refactored createHeading to throw an error instead of creating a heading.

--- utilities/createHeading/index.js
+++ utilities/createHeading/index.js
@@ -1,19 +1,19 @@
 /**
  * Create a heading element
- * @param level number A number from 1 to 6 (inclusive) that corresponds to the level of the heading to be created.
- * @param text string The text to display in the heading.
- * @returns {HTMLElement} The Heading element that can be appended tothe DOM
+ * @param {number} level - A number from 1 to 6 (inclusive) that corresponds to the level of the heading to be created.
+ * @param {string} text - The text to display in the heading.
+ * @returns {HTMLElement} - The Heading element that can be appended tothe DOM
  */
 export default function createHeading (level, text) {
-	let element;
-	if (level < 1) {
-		element = document.createElement('h1');
-	} else if (level > 6) {
-		element = document.createElement('h6');
-	} else {
-		element = document.createElement(`h${level}`)
+	const levelErrorMessage = "The level must be between 1 and 6 inclusive.";
+
+	if (level < 1 || level > 6) {
+		throw Error(levelErrorMessage);
 	}
+
+	const element = document.createElement(`h${level}`);
 
 	element.textContent = text;
 	return element;
+
 }
--- utilities/createHeading/index.test.js
+++ utilities/createHeading/index.test.js
@@ -2,26 +2,26 @@ import createHeading from './index.js';
 import { getByRole } from '@testing-library/dom';
 
 describe('createHeading', () => {
-	it('should create a heading level 1 that displays the text "Hello World!".', () => {
-		const heading = createHeading(1, "Hello World!");
-		const container = document.createElement('div');
-		container.appendChild(heading);
-		expect(getByRole(container, 'heading', {level: 1})).toHaveTextContent("Hello World!")
-	});
-
-	it('should should create a headingl level 1 if the provided level is less than 1',
+	it('should create a heading level 1 that displays the text "Hello World!".',
 			() => {
-				const heading = createHeading(-2, "Hello World!");
-				const container = document.createElement('div');
-				container.appendChild(heading);
-				expect(getByRole(container, 'heading', {level: 1})).toHaveTextContent("Hello World!")
+				const heading = createHeading(1, 'Hello World!');
+				expect(heading).toHaveTextContent('Hello World!');
+			});
+
+	it('should should throw an error if the provided level is less than 1',
+			() => {
+				expect(() => {
+					createHeading(-2, 'Hello World!');
+				}).
+						toThrow('The level must be between 1 and 6 inclusive.');
+
 			});
 
 	it('should create a heading level 6 if the provided level is greater than 6',
 			() => {
-				const heading = createHeading(7, "Hello World!");
-				const container = document.createElement('div');
-				container.appendChild(heading);
-				expect(getByRole(container, 'heading', {level: 6})).toHaveTextContent("Hello World!")
+				expect(() => {
+					createHeading(7, 'Hello World!');
+				}).
+						toThrowError('The level must be between 1 and 6 inclusive.');
 			});
 });
